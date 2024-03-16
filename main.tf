957b65e9f9aea5c8cf5352e245ae833b2db80fa5
Grigoriy Yukhanyan <grigoriy090@gmailcom>
2024-03-16 09:10:57 +0000

    ..

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-# general
+# Определение параметра SSM для AMI Amazon Linux
 data "aws_ssm_parameter" "my_amzn_linux_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-
+# Определение префикса имени экземпляров EC2
 locals {
     ec2_name_prefix = "gryukhanyan_ec2_tf_"
 }
-
 
 # Создание Security Group
 resource "aws_security_group" "sg-tf" {
@@ -39,8 +38,7 @@ resource "aws_security_group" "sg-tf" {
   }
 }
 
-
-# Launch template
+# Создание Launch Template
 resource "aws_launch_template" "launch_template_tf" {
   name_prefix = var.launch_template_name
   description = "EC2 launch template generated from terraform"
@@ -48,21 +46,19 @@ resource "aws_launch_template" "launch_template_tf" {
   network_interfaces {
     device_index                = 0
     associate_public_ip_address = true
-    security_groups             = [data.aws_security_group.sg.id]
+    security_groups             = [aws_security_group.sg-tf.id]
   }
 
   instance_type = var.instance_type_tf
   key_name      = var.key_pair_tf
   image_id      = data.aws_ssm_parameter.my_amzn_linux_ami.insecure_value
 
-
   placement {
     availability_zone = var.availability_zone_tf
   }
 }
 
-
-# EC2
+# Создание экземпляров EC2
 resource "aws_instance" "ec2_tf" {
   count = 3
 
@@ -72,6 +68,6 @@ resource "aws_instance" "ec2_tf" {
   }
 
   tags = {
-    Name = "gryukhanyan_ec2_tf_${format("%01d", count.index + 1)}"
+    Name = "${local.ec2_name_prefix}${format("%01d", count.index + 1)}"
   }
 }
